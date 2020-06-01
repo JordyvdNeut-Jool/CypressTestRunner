c13945dc0d37c3ae1e1405ba2dde1c869806359b
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-06-01 09:00:57 +0200

    changed prepairData += to ES6 string

--- controller/configureFiles.js
+++ controller/configureFiles.js
@@ -1,23 +1,23 @@
 var fs = require('fs');
 var cmd = require('node-cmd');
+var appjs = require('../app.js');
 
+// Variable to save the tests
 const testFolder = "./cypress/integration/examples/";
 
 // Format the code
 module.exports.prepairData = (data) => {
   let name = data.name.split(" ").join("_")
-  filePath = testFolder + name + '.spec.js';
+  
+  filePath = appjs.testFolder + name + '.spec.js';
   prepairedbeforeEach = "";
-  prepairedCode = '';
-  prepairedCode += 'describe("' + data.name + '", ';
-  prepairedCode += 'function(){\n';
-  prepairedCode += '\tbeforeEach(function (){\n';
-  prepairedCode += prepairedbeforeEach + "\n";
-  prepairedCode += '\t})\n';
-  prepairedCode += '\tit("' + data.description + '", function (){\n';
-  prepairedCode += data.code;
-  prepairedCode += '\t})\n';
-  prepairedCode += '})\n';
+  prepairedCode = `describe("${data.name}", function(){
+  beforeEach(function (){
+    ${prepairedbeforeEach}
+  })
+  it("${data.description ? data.description : data.name}", function (){
+${data.code}  })
+})`
   return { filePath, prepairedCode };
 }
 
@@ -26,7 +26,6 @@ module.exports.saveCode = (data) => {
   prepairedData = this.prepairData(data);
   filePath = prepairedData.filePath;
   prepairedCode = prepairedData.prepairedCode;
-
   fs.writeFile(filePath, prepairedCode, function (err) {
     if (err) {
       return console.log('Error: ' + err)
@@ -49,7 +48,7 @@ module.exports.openCypressRunner = () => {
 module.exports.openRecentTest = (code) => {
   let data = {code: code, name: "RecentTest", description: "Last recorded test"};
   this.saveCode(data);
-  var filePath = ".\\cypress\\integration\\examples\\" + data.name + ".spec.js"
+  var filePath = appjs.testFolder + data.name + ".spec.js"
   cmd.get('npx cypress run --spec "' + filePath + '" --browser chrome --no-exit', function (err, data, stderr) {
     if (!err) {
       console.log("opened cypress");
@@ -61,5 +60,5 @@ module.exports.openRecentTest = (code) => {
 
 // read all filenames in $testFolder
 module.exports.readTestNames = () => {
-  return fs.readdirSync(testFolder, "utf-8");
+  return fs.readdirSync(appjs.testFolder, "utf-8");
 }
